d0c6d4ef2c82c74dd10d34f2da69f59206441d0b
Rodrigo <rodrigo.rodriguez@live.com.ar>
2025-01-11 16:52:40 -0300

    update

--- estilodevida/lib/home_page.dart
+++ estilodevida/lib/home_page.dart
@@ -6,6 +6,7 @@ import 'package:estilodevida/ui/widgets/main_buttons.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:in_app_update/in_app_update.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,6 +17,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setUserToCrashlytics();
+      checkForUpdate();
+    });
+  }
+
   Future<void> setUserToCrashlytics() async {
     final user = context.watch<User?>();
     final ref = FirebaseCrashlytics.instance;
@@ -35,10 +46,53 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  @override
-  void didChangeDependencies() {
-    setUserToCrashlytics();
-    super.didChangeDependencies();
+  Future<void> checkForUpdate() async {
+    try {
+      final updateInfo = await InAppUpdate.checkForUpdate();
+
+      if (updateInfo.updateAvailability == UpdateAvailability.updateAvailable) {
+        final result = await InAppUpdate.startFlexibleUpdate();
+
+        if (result == AppUpdateResult.success) {
+          await InAppUpdate.completeFlexibleUpdate();
+        }
+      }
+    } catch (err, stack) {
+      showSnack('Error al actualizar la app');
+      errorHandler(
+        err: err,
+        stack: stack,
+        reason: 'Error Update App',
+        information: [],
+      );
+    }
+  }
+
+  void showSnack(String text) {
+    if (mounted) {
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Row(
+            children: [
+              const Icon(
+                Icons.check_circle,
+                color: Colors.white,
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Text(
+                text,
+                style: const TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+          backgroundColor: Colors.red,
+          duration: const Duration(seconds: 5),
+        ),
+      );
+    }
   }
 
   @override
