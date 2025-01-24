dceed287218238319c2c72bb9edd94bebea26d51
Rodrigo <rodrigo.rodriguez@live.com.ar>
2025-01-24 13:40:45 -0300

    init

--- estilodevida/lib/ui/register_lesson/register_lesson.dart
+++ estilodevida/lib/ui/register_lesson/register_lesson.dart
@@ -3,10 +3,12 @@ import 'package:estilodevida/services/http_service/http_service.dart';
 import 'package:estilodevida/ui/constants.dart';
 import 'package:estilodevida/ui/lessons_avalibles.dart';
 import 'package:estilodevida/ui/register_lesson/qr_scanner.dart';
+import 'package:estilodevida/ui/widgets/register_lesson_animation.dart';
 import 'package:estilodevida/ui/widgets/snackbar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 
 class RegisterLessons extends StatefulWidget {
@@ -18,8 +20,30 @@ class RegisterLessons extends StatefulWidget {
   State<RegisterLessons> createState() => _RegisterLessonsState();
 }
 
-class _RegisterLessonsState extends State<RegisterLessons> {
+class _RegisterLessonsState extends State<RegisterLessons>
+    with SingleTickerProviderStateMixin {
   User? user;
+  OverlayEntry? _overlayEntry;
+  late final AnimationController _animationController;
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Inicializamos el controller con el vsync para manejar la animación
+    _animationController = AnimationController(vsync: this);
+    // Escuchamos el cambio de estado de la animación para remover el overlay cuando termine
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _removeOverlay();
+      }
+    });
+  }
+
   @override
   void didChangeDependencies() {
     user = context.watch<User>();
@@ -36,7 +60,7 @@ class _RegisterLessonsState extends State<RegisterLessons> {
         information: [],
       );
     }
-    if (token == tokenQR) {
+    if (token != tokenQR) {
       registerLesson(user!);
     } else {
       ScaffoldMessenger.of(context).hideCurrentSnackBar();
@@ -68,39 +92,40 @@ class _RegisterLessonsState extends State<RegisterLessons> {
   Future<void> registerLesson(User user) async {
     var response;
     try {
-      response =
-          await HttpService(baseUrl: baseUrl).post(registerLessonEndPoint, {
-        'userId': user.uid,
-        'userName': user.displayName,
-        'userPhoto': user.photoURL,
-        'lesson': widget.lessson.name,
-      });
+      _showOverlay();
+      // response =
+      //     await HttpService(baseUrl: baseUrl).post(registerLessonEndPoint, {
+      //   'userId': user.uid,
+      //   'userName': user.displayName,
+      //   'userPhoto': user.photoURL,
+      //   'lesson': widget.lessson.name,
+      // });
 
-      ScaffoldMessenger.of(context).hideCurrentSnackBar();
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Row(
-            children: [
-              Icon(
-                Icons.check_circle,
-                color: Colors.white,
-              ),
-              SizedBox(
-                width: 5,
-              ),
-              Text(
-                'Gracias por registrar tu clase',
-                style: TextStyle(color: Colors.white),
-              ),
-            ],
-          ),
-          backgroundColor: blue,
-          duration: Duration(seconds: 5),
-        ),
-      );
+      // ScaffoldMessenger.of(context).hideCurrentSnackBar();
+      // ScaffoldMessenger.of(context).showSnackBar(
+      //   const SnackBar(
+      //     content: Row(
+      //       children: [
+      //         Icon(
+      //           Icons.check_circle,
+      //           color: Colors.white,
+      //         ),
+      //         SizedBox(
+      //           width: 5,
+      //         ),
+      //         Text(
+      //           'Gracias por registrar tu clase',
+      //           style: TextStyle(color: Colors.white),
+      //         ),
+      //       ],
+      //     ),
+      //     backgroundColor: blue,
+      //     duration: Duration(seconds: 5),
+      //   ),
+      // );
 
       if (mounted) {
-        GoRouter.of(context).pop();
+        //  GoRouter.of(context).pop();
       }
     } catch (err, stack) {
       errorHandler(
@@ -110,6 +135,44 @@ class _RegisterLessonsState extends State<RegisterLessons> {
         information: [response],
       );
     }
+  }
+
+  void _showOverlay() {
+    if (_overlayEntry != null) return;
+
+    final overlay = Overlay.of(context);
+    _overlayEntry = OverlayEntry(
+      builder: (context) => Positioned.fill(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.black54,
+            ),
+            Center(
+              child: SizedBox(
+                width: 400,
+                height: 400,
+                child: Lottie.asset(controller: _animationController,
+                    onLoaded: (composition) {
+                  _animationController.duration = composition.duration;
+                  _animationController.forward(from: 0);
+                },
+                    repeat: false,
+                    'assets/animations/check_animation.json',
+                    fit: BoxFit.contain),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    overlay.insert(_overlayEntry!);
+  }
+
+  void _removeOverlay() {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
   }
 
   void showMessage() => showCustomSnackBar(context, 'Clase Registrada');
